915074157dce2332e14e4ff0284f24e6a3155100
Saleh Alibrahim <s.albrahim@outlook.com>
2020-06-09 00:25:32 +0300

    done form the backend

--- README.md
+++ README.md
@@ -1,51 +1,44 @@
-# Full Stack Trivia API  Frontend
+# Full Stack API Final Project
 
-## Getting Setup
+## Full Stack Trivia
 
-> _tip_: this frontend is designed to work with [Flask-based Backend](../backend). It is recommended you stand up the backend first, test using Postman or curl, update the endpoints in the frontend, and then the frontend should integrate smoothly.
+Udacity is invested in creating bonding experiences for its employees and students. A bunch of team members got the idea to hold trivia on a regular basis and created a  webpage to manage the trivia app and play the game, but their API experience is limited and still needs to be built out. 
 
-### Installing Dependencies
+That's where you come in! Help them finish the trivia app so they can start holding trivia and seeing who's the most knowledgeable of the bunch. The application must:
 
-#### Installing Node and NPM
+1) Display questions - both all questions and by category. Questions should show the question, category and difficulty rating by default and can show/hide the answer. 
+2) Delete questions.
+3) Add questions and require that they include question and answer text.
+4) Search for questions based on a text query string.
+5) Play the quiz game, randomizing either all questions or within a specific category. 
 
-This project depends on Nodejs and Node Package Manager (NPM). Before continuing, you must download and install Node (the download includes NPM) from [https://nodejs.com/en/download](https://nodejs.org/en/download/).
+Completing this trivia app will give you the ability to structure plan, implement, and test an API - skills essential for enabling your future applications to communicate with others. 
 
-#### Installing project dependencies
+## Tasks
 
-This project uses NPM to manage software dependencies. NPM Relies on the package.json file located in the `frontend` directory of this repository. After cloning, open your terminal and run:
+There are `TODO` comments throughout project. Start by reading the READMEs in:
 
-```bash
-npm install
-```
+1. [`./frontend/`](./frontend/README.md)
+2. [`./backend/`](./backend/README.md)
 
->_tip_: **npm i** is shorthand for **npm install**
+We recommend following the instructions in those files in order. This order will look familiar from our prior work in the course.
 
-## Required Tasks
+## Starting and Submitting the Project
 
-## Running Your Frontend in Dev Mode
+[Fork](https://help.github.com/en/articles/fork-a-repo) the [project repository]() and [Clone](https://help.github.com/en/articles/cloning-a-repository) your forked repository to your machine. Work on the project locally and make sure to push all your changes to the remote repository before submitting the link to your repository in the Classroom. 
 
-The frontend app was built using create-react-app. In order to run the app in development mode use ```npm start```. You can change the script in the ```package.json``` file. 
+## About the Stack
 
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser. The page will reload if you make edits.<br>
+We started the full stack application for you. It is desiged with some key functional areas:
 
-```bash
-npm start
-```
+### Backend
 
-## Request Formatting
+The `./backend` directory contains a partially completed Flask and SQLAlchemy server. You will work primarily in app.py to define your endpoints and can reference models.py for DB and SQLAlchemy setup. 
 
-The frontend should be fairly straightforward and disgestible. You'll primarily work within the ```components``` folder in order to edit the endpoints utilized by the components. While working on your backend request handling and response formatting, you can reference the frontend to view how it parses the responses. 
+### Frontend
 
-After you complete your endpoints, ensure you return to and update the frontend to make request and handle responses appropriately: 
-- Correct endpoints
-- Update response body handling 
+The `./frontend` directory contains a complete React frontend to consume the data from the Flask server. You will need to update the endpoints after you define them in the backend. Those areas are marked with TODO and can be searched for expediency. 
 
-## Optional: Styling
+Pay special attention to what data the frontend is expecting from each API response to help guide how you format your API. 
 
-In addition, you may want to customize and style the frontend by editing the CSS in the ```stylesheets``` folder. 
-
-## Optional: Game Play Mechanics
-
-Currently, when a user plays the game they play up to five questions of the chosen category. If there are fewer than five questions in a category, the game will end when there are no more questions in that category. 
-
-You can optionally update this game play to increase the number of questions or whatever other game mechanics you decide. Make sure to specify the new mechanics of the game in the README of the repo you submit so the reviewers are aware that the behavior is correct. 
+[View the README.md within ./frontend for more details.](./frontend/README.md)
---
+++ backend/__init__.py
@@ -0,0 +1,230 @@
+import os
+from flask import Flask, request, abort, jsonify, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import random
+from models import setup_db, Question, Category
+
+
+app = Flask(__name__)
+setup_db(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+QUESTIONS_PER_PAGE = 10
+
+
+def paginate_questions(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
+
+'''
+  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+'''
+# CORS Headers
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
+'''
+  @TODO: Use the after_request decorator to set Access-Control-Allow
+'''
+
+
+@app.route('/categories')
+def retrieve_categories():
+    try:
+        categorysList = Category.query.order_by(Category.id).all()
+        result = []
+        for category in categorysList:
+            result.append(category.format())
+        return jsonify({
+            'categories': result
+        })
+    except Exception as error:
+        print(error)
+        abort(500)
+        '''
+      @TODO:
+      Create an endpoint to handle GET requests for questions,
+      including pagination (every 10 questions).
+      This endpoint should return a list of questions,
+      number of total questions, current category, categories.
+
+      TEST: At this point, when you start the application
+      you should see questions and categories generated,
+      ten questions per page and pagination at the bottom of the screen for three pages.
+      Clicking on the page numbers should update the questions.
+      '''
+
+
+@app.route('/questions')
+def retrieve_questions():
+    try:
+        questionsList = Question.query.order_by(Question.id).all()
+        categorysList = Category.query.order_by(Category.id).all()
+
+        current_questions = paginate_questions(request, questionsList)
+        current_categorysList = paginate_questions(request, categorysList)
+        result = []
+        for category in categorysList:
+            result.append(category.format())
+        return jsonify({
+            'questions': current_questions,
+            'total_questions': len(current_questions),
+            'categories': "current_categorysList",
+            'current_category': "current_categorysList",
+        })
+    except Exception as error:
+        print(error)
+        abort(500)
+
+
+@app.route('/questions/<int:question_id>', methods=["DELETE"])
+def delete_question(question_id):
+    try:
+        question = Question.query.filter_by(id=question_id).one_or_none()
+        if question is None:
+            abort(404)
+
+        question.delete()
+
+        return redirect(url_for('retrieve_questions'))
+    except Exception as error:
+        print(error)
+        abort(422)
+    '''
+  @TODO:
+  Create an endpoint to POST a new question,
+  which will require the question and answer text,
+  category, and difficulty score.
+
+  TEST: When you submit a question on the "Add" tab,
+  the form will clear and the question will appear at the end of the last page
+  of the questions list in the "List" tab.
+  '''
+
+
+@app.route('/questions', methods=["POST"])
+def create_question():
+    try:
+        body = request.get_json()
+        new_question = body.get('question', None)
+        new_answer = body.get('answer', None)
+        new_category = body.get('category', None)
+        new_difficulty = body.get('difficulty', None)
+        question = Question(question=new_question, answer=new_answer,
+                            category=new_category, difficulty=new_difficulty)
+        question.insert()
+        return redirect(url_for('retrieve_questions'))
+    except Exception as error:
+        print(error)
+        abort(422)
+    '''
+  @TODO: 
+  Create a POST endpoint to get questions based on a search term. 
+  It should return any questions for whom the search term 
+  is a substring of the question. 
+
+  TEST: Search by any phrase. The questions list will update to include 
+  only question that include that string within their question. 
+  Try using the word "title" to start. 
+  '''
+
+
+@app.route('/questions/<string:word>', methods=["POST"])
+def get_questionByWord(word):
+    try:
+        tag = f"%{word}%"
+        questionsList = Question.query.order_by(Question.id).filter(
+            Question.question.like(tag)).all()
+        result = []
+        for question in questionsList:
+            result.append(question.format())
+        return jsonify({
+            'success': True,
+            'questions': result,
+            'total_questions': len(result)
+        })
+    except Exception as error:
+        print(error)
+        abort(422)
+    '''
+  @TODO: 
+  Create a GET endpoint to get questions based on category. 
+
+  TEST: In the "List" tab / main screen, clicking on one of the 
+  categories in the left column will cause only questions of that 
+  category to be shown. 
+  '''
+
+
+@app.route('/categories/<int:id>/questions', methods=["GET"])
+def get_questionByCategory(id):
+    try:
+        questionsList = Question.query.order_by(Question.id).filter(
+            Question.category == id).all()
+        result = []
+        for question in questionsList:
+            result.append(question.format())
+        return jsonify({
+            'success': True,
+            'questions': result,
+            'total_questions': len(result)
+        })
+    except Exception as error:
+        print(error)
+        abort(422)
+
+    '''
+  @TODO: 
+  Create a POST endpoint to get questions to play the quiz. 
+  This endpoint should take category and previous question parameters 
+  and return a random questions within the given category, 
+  if provided, and that is not one of the previous questions. 
+
+  TEST: In the "Play" tab, after a user selects "All" or a category,
+  one question at a time is displayed, the user is allowed to answer
+  and shown whether they were correct or not. 
+  '''
+
+
+# @app.route('/questions/<string:category>', methods=["GET"])
+# def get_questionByCategory(category):
+#     try:
+#         questionsList = Question.query.order_by(Question.id).filter(
+#             Question.category == category).all()
+#         result = []
+#         for question in questionsList:
+#             result.append(question.format())
+#         return jsonify({
+#             'success': True,
+#             'questions': result,
+#             'total_questions': len(result)
+#         })
+#     except Exception as error:
+#         print(error)
+#         abort(422)
+    '''
+  @TODO: 
+  Create error handlers for all expected errors 
+  including 404 and 422. 
+  '''
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
